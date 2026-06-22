a133fb2b0305a1935fc7a58168c0189f09ce2617
Kurra Dinesh <kurradinesh93@gmail.com>
2026-06-22 07:45:39 +0530

    Updated portfolio website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -73,7 +73,7 @@ export default function Hero() {
           >
             <div className="profile-shell position-relative d-inline-block fade-in-right">
               <div className="profile-ring" />
-             <img src="/Dinesh-portfolio/src/assets/Dinesh.jpeg" alt="Kurra Dinesh Profile" className="img-fluid rounded-circle hero-profile-avatar" />
+             <img src={`${import.meta.env.BASE_URL}Dinesh.jpeg`} alt="Kurra Dinesh Profile" className="img-fluid rounded-circle hero-profile-avatar" />
               {floatingTech.map((item, index) => (
                 <motion.span
                   key={item.label}
